1fdbf29c02af2fb5a7c12502e0816af48efc370d
IVanchekus <ivan.gravdan@mail.ru>
2024-10-02 14:41:33 +0700

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace MyWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyTextBox.Text = "Button Clicked!";
+        }
+
+        private void MyComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (MyComboBox.SelectedItem is System.Windows.Controls.ComboBoxItem selectedItem)
+            {
+                MyTextBox.Text = $"Selected: {selectedItem.Content}";
+            }
+        }
+    }
+}
